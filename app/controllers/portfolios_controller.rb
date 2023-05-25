5549af38cad88caeefca6b185c5c16337a94ed6c
Igor Alves arantes <iarantes@bioritmo.com.br>
2023-05-25 08:38:19 -0300

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,10 +12,28 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: "Yur Portfolio item is now live." }
+        format.html { redirect_to portfolios_path, notice: "Your Portfolio item is now live." }
         format.json { render :show, status: :created, location: @portfolio_item }
       else
         format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "The record succesfully updated" }
+        format.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
       end
     end
