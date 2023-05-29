bb47d246cc754335ebc899e332ce15afdf64beba
Igor Alves arantes <iarantes@bioritmo.com.br>
2023-05-29 11:10:41 -0300

    Implemented concern for managing image data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_123458) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_135809) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_123458) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
