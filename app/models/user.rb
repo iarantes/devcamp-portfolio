b5ca48e0de008860e3dbdc9242d39ea53415b968
Igor Alves arantes <iarantes@bioritmo.com.br>
2023-05-30 08:20:55 -0300

    Completed authentication with virtual attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    total = self.name.split.count - 1
+    string = ''
+    count = 0
+    self.name.split.last(total).each do |s|
+      count+=1
+
+      if count == 1
+        string << "#{s}"
+      else
+        string << " #{s}"
+      end
+    end
+    return string
+  end
 end
