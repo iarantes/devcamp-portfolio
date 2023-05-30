dabe1bb650b1fc7fdb5f924a74f1d1510b9290ae
Igor Alves arantes <iarantes@bioritmo.com.br>
2023-05-30 14:31:44 -0300

    Added guest user for current_user

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,6 +22,7 @@ class BlogsController < ApplicationController
   # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
+    @blog.topic_id = Topic.last.id
 
     respond_to do |format|
       if @blog.save
