632b6296dc83bb84e60401b4c903c1c3c45507f0
Igor Alves arantes <iarantes@bioritmo.com.br>
2023-06-06 09:28:17 -0300

    Updated master layout styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper style
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path) +
-      "<br>".html_safe +
-      (link_to 'Login', new_user_session_path)
+      (link_to 'Register', new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to 'Login', new_user_session_path, class: style)
     else
-      link_to 'Logout', destroy_user_session_path, data: { turbo_method: :delete, turbo_confirm: 'Are you sure?' }
+      link_to 'Logout', destroy_user_session_path, data: { turbo_method: :delete, turbo_confirm: 'Are you sure?' }, class: style
     end
   end
 
