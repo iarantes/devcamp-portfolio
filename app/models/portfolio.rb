d422df945f1b9bf271731e18e8322f7a4c5d9036
Igor Alves arantes <iarantes@bioritmo.com.br>
2023-06-28 13:48:03 -0300

    Updated portfolio to include attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -10,6 +10,10 @@ class Portfolio < ApplicationRecord
     where(subtitle: 'Angular')
   end
 
+  def self.bi_position
+    order("position ASC")
+  end
+
   scope :ruby_on_rails, -> {where(subtitle: 'Ruby on Rails')}
 
   after_initialize :set_defaults
